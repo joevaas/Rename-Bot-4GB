7df55f373e7f3a9a8a5b122a84d01a421151f617
AniShin21 <minecraft2727k@gmail.com>
2024-07-15 17:22:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,8 +20,8 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Tamil Anime</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/Tamil_Anime_Files'>AniShin And Moon Walker</a>
 
 <b>♻️ Bot Made By :</b> @Madflix_Bots"""
 
@@ -30,7 +30,7 @@ class script(object):
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🛍 UPI ID:</b> <a href='https://t.me/Tamil_Aime_Files'>Contact Here</a> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
